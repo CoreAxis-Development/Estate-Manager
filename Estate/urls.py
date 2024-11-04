a31cade3b8b86a70b3d282cf004c2b8648ccf03c
KeaganGilmore <keagangilmore@gmail.com>
2024-11-04 22:31:09 +0200

    initial login setup

--- Estate/urls.py
+++ Estate/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('CheckList.urls')),
     path('', include('AssetManager.urls')),
-    path('', include('UserManagement.urls')),  # Add this line
+    path('', include('UserManagement.urls')),
 ]
